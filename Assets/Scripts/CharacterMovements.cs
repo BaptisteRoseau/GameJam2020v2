2ae1b6faa302a5b780e5be2eeac5f1d67341355c
Manaut <manaut.bertonboitel@mmibordeaux.com>
2020-01-18 05:03:13 +0100

    Shoot System

--- Assets/Scripts/CharacterMovements.cs
+++ Assets/Scripts/CharacterMovements.cs
@@ -5,58 +5,63 @@ using UnityEngine;
 public class CharacterMovements : MonoBehaviour
 {
     public float speed;
+    public float crosshairSpeed;
+    private float crosshairMovement = 0f;
     private float moveInputH;
     private float moveInputV;
+    public GameObject crosshair;
 
     public Rigidbody2D rb;
 
-    /*public GameObject ballPrefab;
-    public GameObject crosshair;
-    public float arrowSpeed;*/
+    public GameObject ballPrefab;
+    public float arrowSpeed;
     public int num_player;
 
-    //public bool shooted;
 
     void Start()
     {
-        //shooted = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    /*private void Update()
-    {
-        if (Input.GetButtonDown("Fire") && shooted == false)
-        {
-            shooted = true;
-            Shoot();
-        }
-    }*/
-
     void FixedUpdate()
     {
+
+
         if (num_player == 1)
         {
+            if (Input.GetButtonDown("Fire"))
+            {
+                Shoot();
+            }
             moveInputH = Input.GetAxis("Vertical");
             moveInputV = Input.GetAxis("Horizontal");
+            crosshairMovement = Input.GetAxisRaw("CrosshairMove");
         }
         else if (num_player == 2)
         {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                Shoot();
+            }
             moveInputH = -Input.GetAxis("Vertical2");
             moveInputV = -Input.GetAxis("Horizontal2");
+            crosshairMovement = Input.GetAxisRaw("CrosshairMove2");
         }
 
         rb.velocity = new Vector2(moveInputH * speed, rb.velocity.y);
         rb.velocity = new Vector2(moveInputV * speed, rb.velocity.x);
+
+        crosshair.transform.RotateAround(this.transform.position, Vector3.forward, crosshairMovement * Time.fixedDeltaTime * -crosshairSpeed);
     }
 
-    /*void Shoot()
+    void Shoot()
     {
         Vector2 shootingDirection = crosshair.transform.localPosition;
         shootingDirection.Normalize();
 
-        GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        GameObject ball = Instantiate(ballPrefab, crosshair.transform.position, Quaternion.identity);
         ball.GetComponent<Rigidbody2D>().velocity = shootingDirection * arrowSpeed;
-        Destroy(ball, 2.0f);
+        Destroy(ball, 1f);
     
-    }*/
+    }
 }
