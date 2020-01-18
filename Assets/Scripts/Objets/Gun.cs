16da90bb8fe52b659bafaa6497511d6b8c90ac93
Baptiste ROSEAU <broseau@gmail.com>
2020-01-18 14:23:11 +0200

    Added some sprites

--- Assets/Scripts/Objets/Gun.cs
+++ Assets/Scripts/Objets/Gun.cs
@@ -14,12 +14,14 @@ public class Gun : PowerUp
     {
         if (Input.GetButtonDown(player.GetComponent<Player>().fireCommand))
         {
-            GameObject crosshair = player.GetComponent<Player>().crosshair;
+            Player playerComponenent = player.GetComponent<Player>();
+            GameObject crosshair = playerComponenent.crosshair;
             Vector2 shootingDirection = crosshair.transform.position - player.transform.position;
             shootingDirection.Normalize();
 
             Belette ball = Instantiate(belettePrefab, crosshair.transform.position, Quaternion.identity).GetComponent<Belette>();
-            ball.fromPlayer = player.GetComponent<Player>().num_player;
+            ball.fromPlayer = playerComponenent.num_player;
+            ball.transform.rotation = playerComponenent.transform.rotation;
             ball.GetComponent<Rigidbody2D>().velocity = shootingDirection * arrowSpeed;
             Destroy(ball, 1f);
         }
