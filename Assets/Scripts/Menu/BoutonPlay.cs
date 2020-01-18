02bc1c30d38ed50472c1a81d342bc1ea39af14c3
Maelle05 <maelle.rabouan@gmail.com>
2020-01-18 21:33:32 +0100

    menu v1

---
+++ Assets/Scripts/Menu/BoutonPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoutonPlay : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT !");
+        Application.Quit(); 
+    }
+}
