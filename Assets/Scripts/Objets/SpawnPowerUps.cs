b70bff8ac7600ce311231cdfd20f5b0c282377d3
Baptiste ROSEAU <broseau@gmail.com>
2020-01-19 01:10:29 +0200

    Fixed PowerUp Spawner

--- Assets/Scripts/Objets/SpawnPowerUps.cs
+++ Assets/Scripts/Objets/SpawnPowerUps.cs
@@ -9,24 +9,11 @@ public class SpawnPowerUps : MonoBehaviour
     public GameObject[] objects;       // Object to be spawned
     public Transform[] spawnPoints;
 
-
-    // Similar to InvokeRepeating but with a random wait between minTime and maxTime
-    public delegate void MethodToCall(); //A delegate - you can pass in any method with the signature void xxx() in this case!
-    public void InvokeRepeatingRange(MethodToCall method, float timeUntilStart, float minTime, float maxTime)
-    {
-        
-    }
-
-    // Start is called before the first frame update
-    /*void Start()
-    {
-
-    }*/
+    private float nextCall = 0.0f;
 
     private void Update()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        float nextCall = 0.0f;
         while (Time.realtimeSinceStartup > nextCall)
         {
             Debug.Log("Called");
