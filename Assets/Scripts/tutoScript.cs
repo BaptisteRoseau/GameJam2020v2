e814d02b9736daf621ab80e5adb34d76ba5d1180
Dupuis <calibandj@gamil.com>
2020-01-19 13:52:37 +0100

    yes tuto

---
+++ Assets/Scripts/tutoScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void run()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
