18fec7ec37cf795f01a3126fe617af048c12fecd
Dupuis <calibandj@gamil.com>
2020-01-19 15:15:07 +0100

    Advertising

--- Assets/Scripts/tutoScript.cs
+++ Assets/Scripts/tutoScript.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class tutoScript : MonoBehaviour
 {
+
+    private GameObject score ;
+    private GameObject moon ;
+
     // Start is called before the first frame update
     void Start()
     {
+        score =  GameObject.FindGameObjectWithTag("scoresUi");
+        score.SetActive(false);
+        moon = GameObject.FindGameObjectWithTag("moonUi");
+        moon.SetActive(false);
         Time.timeScale = 0;
+        
     }
 
     // Update is called once per frame
@@ -18,7 +27,11 @@ public class tutoScript : MonoBehaviour
 
     public void run()
     {
+        score.SetActive(true);
+        moon.SetActive(true);
         Time.timeScale = 1;
+
         gameObject.SetActive(false);
+        
     }
 }
