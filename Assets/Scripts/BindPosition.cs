9f2612a660fa973f434a0ea39fa79c36b4a88ff8
Baptiste ROSEAU <broseau@gmail.com>
2020-01-19 10:25:06 +0200

    J'abandonne

---
+++ Assets/Scripts/BindPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindPosition : MonoBehaviour
+{
+    public Transform Obj;
+    public Transform Cam;
+    void Update()
+    {
+        transform.position = Obj.position;
+        transform.rotation = Quaternion.Euler(0, Cam.rotation.eulerAngles.y, 0);
+    }
+}
---
+++ Assets/Scripts/DisableRender.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRender : MonoBehaviour
+{
+    public string cameraDisabledName = "null";
+    public Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void OnWillRenderObject()
+    {
+        if (Camera.current.name == cameraDisabledName)
+        {
+            //gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            //gameObject.GetComponent<Renderer>().enabled = true;
+            gameObject.SetActive(true);
+        }
+        Debug.Log(Camera.current.name);
+    }
+}
