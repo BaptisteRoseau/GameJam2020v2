79306573a1c8762b311cfc7a36da56e5257b2878
Dupuis <calibandj@gamil.com>
2020-01-19 01:02:35 +0100

    Début menu

---
+++ Assets/Scripts/ConfigMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigMenu : MonoBehaviour
+{
+    // Player commands
+    public string menuCommand = "Menu";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(menuCommand))
+        {
+            Debug.Log("jjkh");
+        }
+    }
+}
